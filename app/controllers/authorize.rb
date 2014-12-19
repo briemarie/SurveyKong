a0041f2e367f2018ede1e0bf25a954b1eed4126d
juan267 & Valerie Smith <pair+juan267.valeriesmith@devbootcamp.com>
2014-12-19 14:58:07 -0800

    work in progress

--- app/controllers/authorize.rb
+++ app/controllers/authorize.rb
@@ -2,9 +2,55 @@ get '/' do
   erb :"auth/auth"
 end
 
-
-post '/' do
-  if authorized
-  redirect erb :"auth/auth/profile"
-  end
+get '/users/:id' do
+  @user = User.find(params[:id])
+  @surveys = @user.surveys
+  @created = @user.created_surveys
+  erb :profile
 end
+
+get '/users/:id/surveys' do
+  @user = User.find(params[:id])
+  erb :_survey, layout: false
+end
+
+get '/users/:id/surveys/:survey_id' do
+  @user = User.find(params[:id])
+  @survey = Survey.find(params[:survey_id])
+  erb :create_survey
+end
+
+get '/users/:id/surveys/:survey_id/questions' do
+  @user = User.find(params[:id])
+  @survey = Survey.find(params[:survey_id])
+  erb :_question, layout: false
+end
+
+get '/users/:id/surveys/questions/:id/choices' do
+  @user = User.find(params[:id])
+  erb :_question, layout: false
+end
+
+# post '/users/:id/surveys/questions'do
+
+
+
+
+post '/users/:id/surveys' do
+  @user = User.find(params[:id])
+  @survey = @user.created_surveys.create(title:params[:title])
+  content_type :json
+  {user: @user.id, survey: @survey.title}.to_json
+  # redirect "/users/#{@user.id}"
+end
+
+# get '/test' do
+#  "<form id='question' action='/test' method='post'>
+#     <input type='text' name='testt[1][1]' placeholder='content'>
+#     <input type='text' name='testt[2][1]' placeholder='content'>
+#     <input type= 'submit' value='test submit'>
+#   </form>"
+# end
+# post '/test' do
+#   params[:testt]["1"]["1"].to_s
+# end
--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,68 @@
 
 $(document).ready(function () {
 
+  $("#question").on("click", function(event) {
+    event.preventDefault();
+
+    var url = $(this).attr('action')
+    var data = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: url ,
+      data: data
+    }).done(function(response) {
+      $("#choice-container").append(response)
+    })
+  })
+
+  $("#create_survey").on("click", function(event) {
+    event.preventDefault();
+
+    var url =$(this).attr('href')
+
+     $.ajax({
+       type: "GET",
+       url: url
+     }).done(function(response) {
+       console.log(response)
+        $("#survey_container").append(response)
+     })
+  })
+
+  $("#add_question").on("click", function(event) {
+    event.preventDefault();
+
+     var url = $(this).attr('href')
+
+     $.ajax({
+       type: "GET",
+       url: url
+     }).done(function(response) {
+        $("#questions-container").append(response)
+     })
+  })
+
+
+
+  $("#create_survey_form").on("submit", function(event){
+    event.preventDefault();
+    var url = $(this).attr('action');
+    var data = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data : data
+    }).done(function(response) {
+      $('#surveys_created').append("<li>"+response.survey+"</li>");
+    })
+
+  })
   // send an HTTP DELETE request for the sign-out link
-  $('a#sign-out').on("click", function (e) {
-    e.preventDefault();
-    var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
-    request.done(function () { window.location = "/"; });
-  });
+  // $('a#sign-out').on("click", function (e) {
+  //   e.preventDefault();
+  //   var request = $.ajax({ url: $(this).attr('href'), type: 'delete' });
+  //   request.done(function () { window.location = "/"; });
+  // });
 });
