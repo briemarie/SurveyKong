74e773f9bea5f57c79e68adfec3688d6ef205283
briemarie <thebriemarie@gmail.com>
2014-12-18 17:32:41 -0800

    Fix issues that occured with merge

--- config/environment.rb
+++ config/environment.rb
@@ -16,22 +16,14 @@ require 'active_record'
 require 'logger'
 
 require 'sinatra'
-
 require "sinatra/reloader" if development?
 
 require 'erb'
-
-require 'date'
-require 'validates_email_format_of'
-
-require 'bcrypt'
-
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
-
 
 configure do
   # By default, Sinatra assumes that the root is the file that calls the configure block.
@@ -45,9 +37,11 @@ configure do
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
 
+
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
